6655685a1b38c7559d97b7cda3739f49d9e715b5
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-02 10:59:10 -0700

    Update installer.bat

--- windows/installer.bat
+++ windows/installer.bat
@@ -2,17 +2,30 @@ call winget upgrade --all
 call winget install -e --id Malwarebytes.Malwarebytes
 call winget install -e --id Microsoft.WindowsTerminal
 call winget install -e --id Piriform.CCleaner
+call winget install -e --id Logitech.GHUB
+
 call winget install -e --id Mozilla.Firefox
 call winget install -e --id Google.Chrome
-call winget install -e --id SublimeHQ.SublimeText.4
-call winget install -e --id Microsoft.VisualStudioCode
-call winget install -e --id OpenJS.NodeJS.LTS
-call winget install -e --id GoLang.Go.1.20
-call winget install -e --id JetBrains.IntelliJIDEA.Community
-call winget install -e --id Oracle.JavaRuntimeEnvironment
-call winget install -e --id Oracle.JDK.17
-call winget install -e --id Docker.DockerDesktop
+
 call winget install -e --id Git.Git
 call winget install -e --id GitHub.GitHubDesktop
-call winget install -e --id Logitech.GHUB
-call winget install -e --id RuneLite.RuneLite
+
+@REM call winget install -e --id Microsoft.VisualStudio.2022.Community.Preview
+call winget install -e --id Microsoft.VisualStudioCode
+call winget install -e --id JetBrains.IntelliJIDEA.Community
+call winget install -e --id SublimeHQ.SublimeText.4
+
+call winget install -e --id Yarn.Yarn
+
+call winget install -e --id Docker.DockerDesktop
+call winget install -e --id Python.Python.3.11
+call winget install -e --id OpenJS.NodeJS.LTS
+call winget install -e --id GoLang.Go.1.20
+call winget install -e --id Oracle.JavaRuntimeEnvironment
+
+@REM call winget install -e --id Oracle.JDK.17
+
+call winget install -e --id GnuPG.GnuPG
+call winget install -e --id AntibodySoftware.WizTree
+
+@REM call winget install -e --id RuneLite.RuneLite
