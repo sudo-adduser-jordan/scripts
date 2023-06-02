57c4af2f8398f316b9a73c5d7689c4e128f021b2
sudo-adduser-jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-02 11:18:29 -0700

    Update installer.bat

--- windows/installer.bat
+++ windows/installer.bat
@@ -15,17 +15,19 @@ call winget install -e --id Microsoft.VisualStudioCode
 call winget install -e --id JetBrains.IntelliJIDEA.Community
 call winget install -e --id SublimeHQ.SublimeText.4
 
-call winget install -e --id Yarn.Yarn
-
 call winget install -e --id Docker.DockerDesktop
-call winget install -e --id Python.Python.3.11
-call winget install -e --id OpenJS.NodeJS.LTS
-call winget install -e --id GoLang.Go.1.20
-call winget install -e --id Oracle.JavaRuntimeEnvironment
+call winget install -e --id Oracle.VirtualBox
 
-@REM call winget install -e --id Oracle.JDK.17
+call winget install -e --id Oracle.JavaRuntimeEnvironment
+call winget install -e --id Python.Python.3.11
+call winget install -e --id GoLang.Go.1.20
+call winget install -e --id OpenJS.NodeJS.LTS
+
+call winget install -e --id Yarn.Yarn
+call winget install -e --id pnpm.pnpm
 
 call winget install -e --id GnuPG.GnuPG
 call winget install -e --id AntibodySoftware.WizTree
+call winget install -e --id Balena.Etcher
 
 @REM call winget install -e --id RuneLite.RuneLite
