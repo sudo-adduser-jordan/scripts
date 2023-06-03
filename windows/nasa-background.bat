975e5af627279550a087ca24a69e74c1d027f519
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-03 10:59:38 -0700

    update

--- windows/nasa-background.bat
+++ windows/nasa-background.bat
@@ -1,2 +1,33 @@
+@REM  ╭─────────────────────────────────────.★..─╮
+@REM               *Custom Script*
+@REM  ╰─..★.─────────────────────────────────────╯
+
+
+@REM          _.-'78o``"`--._
+@REM      ,o888o.  .o888o,   ''-.
+@REM    ,88888P  `78888P..______.]
+@REM   /_..__..----""        __.'
+@REM   `-._       /""| _..-''
+@REM       "`-----\  `\
+@REM               |   ;.-""--..
+@REM               | ,8o.  o88. `.
+@REM               `;888P  `788P  :
+@REM         .o""-.|`-._         ./
+@REM        J88 _.-/    ";"-P----'
+@REM        `--'\`|     /  /
+@REM            | /     |  |
+@REM            \|     /   |akn
+@REM             `-----`---'
+
+@REM  "A byte of time is a byte of gold,
+@REM   but you can’t buy that byte of time,
+@REM   with a byte of gold.”
+
+@REM  (∩｀-´)⊃━ ☆ﾟ.*･｡ﾟ
+
+@REM  ╭─────────────────────────────────────.★..─╮
+@REM                    *Run*
+@REM  ╰─..★.─────────────────────────────────────╯
+
 cd C:\Users\user 1\Documents\GitHub\NASA-Background\Terminal
 python main.py
--- windows/uninstall.bat
+++ windows/uninstall.bat
@@ -1,3 +1,34 @@
+@REM  ╭─────────────────────────────────────.★..─╮
+@REM               *Uninstall Script*
+@REM  ╰─..★.─────────────────────────────────────╯
+
+
+@REM          _.-'78o``"`--._
+@REM      ,o888o.  .o888o,   ''-.
+@REM    ,88888P  `78888P..______.]
+@REM   /_..__..----""        __.'
+@REM   `-._       /""| _..-''
+@REM       "`-----\  `\
+@REM               |   ;.-""--..
+@REM               | ,8o.  o88. `.
+@REM               `;888P  `788P  :
+@REM         .o""-.|`-._         ./
+@REM        J88 _.-/    ";"-P----'
+@REM        `--'\`|     /  /
+@REM            | /     |  |
+@REM            \|     /   |akn
+@REM             `-----`---'
+
+@REM  "A byte of time is a byte of gold,
+@REM   but you can’t buy that byte of time,
+@REM   with a byte of gold.”
+
+@REM  (∩｀-´)⊃━ ☆ﾟ.*･｡ﾟ
+
+@REM  ╭─────────────────────────────────────.★..─╮
+@REM                   *Uninstall*
+@REM  ╰─..★.─────────────────────────────────────╯
+
 call winget remove Microsoft.GetHelp_8wekyb3d8bbwe
 call winget remove Microsoft.MixedReality.Portal_8wekyb3d8bbwe
 call winget remove Microsoft.SkypeApp_kzf8qxf38zg5c
@@ -15,3 +46,51 @@ call winget remove Disney.37853FC22B2CE_6rarf9sa4v8jt
 call winget remove SpotifyAB.SpotifyMusic_zpdnekdrzrea0
 @REM taskkill /f /im OneDrive.exe
 call winget remove Microsoft.OneDrive
+
+@REM  ╭─────────────────────────────────────.★..─╮
+@REM                   *End*
+@REM  ╰─..★.─────────────────────────────────────╯
+
+@echo off
+goto :BEGIN
+
+:::art. 
+:::art.          _.-'78o``"`--._
+:::art.      ,o888o.  .o888o,   ''-.
+:::art.    ,88888P  `78888P..______.]
+:::art.   /_..__..----""        __.'
+:::art.   `-._       /""| _..-''
+:::art.       "`-----\  `\
+:::art.               |   ;.-""--..
+:::art.               | ,8o.  o88. `.
+:::art.               `;888P  `788P  :
+:::art.         .o""-.|`-._         ./
+:::art.        J88 _.-/    ";"-P----'
+:::art.        `--'\`|     /  /
+:::art.            | /     |  |
+:::art.            \|     /   |akn
+:::art.             `-----`---'
+:::art. 
+:::art.  "A byte of time is a byte of gold,
+:::art.   but you can’t buy that byte of time,
+:::art.   with a byte of gold.”
+:::art. 
+:::art.  (∩｀-´)⊃━ ☆ﾟ.*･｡ﾟ
+:::art. 
+
+:BEGIN
+set "BATCH=%~f0"
+
+call :SUB "art"
+> nul pause
+
+:SUB "name"
+for /F "delims=" %%L in ('
+    findstr /L /I /B /C:":::%~1." "%BATCH%"
+') do (
+    set "LINE=%%L"
+    setlocal EnableDelayedExpansion
+    echo(!LINE:*.=!
+    endlocal
+)
+exit /B
