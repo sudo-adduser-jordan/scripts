0981500a6199281054cbca32d075ff760c89734d
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-03 10:09:25 -0700

    Update uninstall.bat

--- windows/uninstall.bat
+++ windows/uninstall.bat
@@ -1,4 +1,3 @@
-call winget remove Disney.37853FC22B2CE_6rarf9sa4v8jt
 call winget remove Microsoft.GetHelp_8wekyb3d8bbwe
 call winget remove Microsoft.MixedReality.Portal_8wekyb3d8bbwe
 call winget remove Microsoft.SkypeApp_kzf8qxf38zg5c
@@ -11,4 +10,8 @@ call winget remove Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
 call winget remove Microsoft.Office.OneNote_8wekyb3d8bbwe
 call winget remove Microsoft.Getstarted_8wekyb3d8bbwe
 call winget remove Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
+call winget remove microsoft.windowscommunicationsapps_8wekyb3d8bbwe
+call winget remove Disney.37853FC22B2CE_6rarf9sa4v8jt
 call winget remove SpotifyAB.SpotifyMusic_zpdnekdrzrea0
+@REM taskkill /f /im OneDrive.exe
+call winget remove Microsoft.OneDrive
