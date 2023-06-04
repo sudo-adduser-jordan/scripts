f97c487dc0b8dd4540c6a6a1ac80fa3c64e96b0a
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-04 15:35:48 -0700

    Update install.bat

--- windows/install.bat
+++ windows/install.bat
@@ -30,41 +30,41 @@
 @REM  ╰─..★.─────────────────────────────────────╯
 
 call winget upgrade --all
-call winget install -e --id Microsoft.WindowsTerminal
-call winget install -e --id Microsoft.PowerToys
+call winget install -e --id Microsoft.WindowsTerminal --accept-package-agreements --accept-source-agreements
+call winget install -e --id Microsoft.PowerToys --accept-package-agreements --accept-source-agreements
 
-call winget install -e --id Mozilla.Firefox
-call winget install -e --id Google.Chrome
+call winget install -e --id Mozilla.Firefox --accept-package-agreements --accept-source-agreements
+call winget install -e --id Google.Chrome --accept-package-agreements --accept-source-agreements
 
-call winget install -e --id Git.Git
-call winget install -e --id GitHub.GitHubDesktop
+call winget install -e --id Git.Git --accept-package-agreements --accept-source-agreements
+call winget install -e --id GitHub.GitHubDesktop --accept-package-agreements --accept-source-agreements
 
-@REM call winget install -e --id Microsoft.VisualStudio.2022.Community.Preview
-call winget install -e --id Microsoft.VisualStudioCode
-call winget install -e --id JetBrains.IntelliJIDEA.Community
-@REM call winget install -e --id SublimeHQ.SublimeText.4
-call winget install -e --id JackieLiu.NotepadsApp
+@REM call winget install -e --id Microsoft.VisualStudio.2022.Community.Preview --accept-package-agreements --accept-source-agreements
+call winget install -e --id Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+call winget install -e --id JetBrains.IntelliJIDEA.Community --accept-package-agreements --accept-source-agreements
+@REM call winget install -e --id SublimeHQ.SublimeText.4 --accept-package-agreements --accept-source-agreements
+call winget install -e --id JackieLiu.NotepadsApp --accept-package-agreements --accept-source-agreements
 
-call winget install -e --id Docker.DockerDesktop
-call winget install -e --id Oracle.VirtualBox
+call winget install -e --id Docker.DockerDesktop --accept-package-agreements --accept-source-agreements
+@REM call winget install -e --id Oracle.VirtualBox --accept-package-agreements --accept-source-agreements
 
-call winget install -e --id Oracle.JavaRuntimeEnvironment
-call winget install -e --id Python.Python.3.11
-call winget install -e --id GoLang.Go.1.20
-call winget install -e --id OpenJS.NodeJS.LTS
+call winget install -e --id Oracle.JavaRuntimeEnvironment --accept-package-agreements --accept-source-agreements
+call winget install -e --id Python.Python.3.11 --accept-package-agreements --accept-source-agreements
+call winget install -e --id GoLang.Go.1.20 --accept-package-agreements --accept-source-agreements
+call winget install -e --id OpenJS.NodeJS.LTS --accept-package-agreements --accept-source-agreements
 
-call winget install -e --id Yarn.Yarn
-call winget install -e --id pnpm.pnpm
+call winget install -e --id Yarn.Yarn --accept-package-agreements --accept-source-agreements
+call winget install -e --id pnpm.pnpm --accept-package-agreements --accept-source-agreements
 
-call winget install -e --id GnuPG.GnuPG
-call winget install -e --id AntibodySoftware.WizTree
-call winget install -e --id Balena.Etcher
-@REM call winget install -e --id ventoy.Ventoy
+call winget install -e --id GnuPG.GnuPG --accept-package-agreements --accept-source-agreements
+call winget install -e --id AntibodySoftware.WizTree --accept-package-agreements --accept-source-agreements
+call winget install -e --id Balena.Etcher --accept-package-agreements --accept-source-agreements
+@REM call winget install -e --id ventoy.Ventoy --accept-package-agreements --accept-source-agreements
 
-@REM call winget install -e --id RuneLite.RuneLite
-call winget install -e --id Piriform.CCleaner
-call winget install -e --id Logitech.GHUB
-call winget install -e --id Malwarebytes.Malwarebytes
+@REM call winget install -e --id RuneLite.RuneLite --accept-package-agreements --accept-source-agreements
+call winget install -e --id Piriform.CCleaner --accept-package-agreements --accept-source-agreements
+call winget install -e --id Logitech.GHUB --accept-package-agreements --accept-source-agreements
+call winget install -e --id Malwarebytes.Malwarebytes --accept-package-agreements --accept-source-agreements
 
 @REM  ╭─────────────────────────────────────.★..─╮
 @REM                    *End*
@@ -110,5 +110,6 @@ for /F "delims=" %%L in ('
     setlocal EnableDelayedExpansion
     echo(!LINE:*.=!
     endlocal
+    )
 )
 exit /B
