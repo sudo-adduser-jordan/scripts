77c1f6d8ea8817938e7976c25c239e2c6b7c01a6
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-04 15:34:38 -0700

    Update uninstall.bat

--- windows/uninstall.bat
+++ windows/uninstall.bat
@@ -29,23 +29,25 @@
 @REM                   *Uninstall*
 @REM  ╰─..★.─────────────────────────────────────╯
 
-call winget remove Microsoft.GetHelp_8wekyb3d8bbwe
-call winget remove Microsoft.MixedReality.Portal_8wekyb3d8bbwe
-call winget remove Microsoft.SkypeApp_kzf8qxf38zg5c
-call winget remove Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
-call winget remove Microsoft.YourPhone_8wekyb3d8bbwe
-call winget remove Microsoft.ZuneVideo_8wekyb3d8bbwe
-call winget remove Microsoft.549981C3F5F10_8wekyb3d8bbwe
-call winget remove Microsoft.WindowsMaps_8wekyb3d8bbwe
-call winget remove Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
-call winget remove Microsoft.Office.OneNote_8wekyb3d8bbwe
-call winget remove Microsoft.Getstarted_8wekyb3d8bbwe
-call winget remove Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
-call winget remove microsoft.windowscommunicationsapps_8wekyb3d8bbwe
-call winget remove Disney.37853FC22B2CE_6rarf9sa4v8jt
-call winget remove SpotifyAB.SpotifyMusic_zpdnekdrzrea0
+call winget remove Microsoft.GetHelp_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.MixedReality.Portal_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.SkypeApp_kzf8qxf38zg5c --accept-source-agreements
+call winget remove Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.YourPhone_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.ZuneVideo_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.549981C3F5F10_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.WindowsMaps_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.Office.OneNote_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.Getstarted_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe --accept-source-agreements
+call winget remove microsoft.windowscommunicationsapps_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.WindowsAlarms_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Microsoft.People_8wekyb3d8bbwe --accept-source-agreements
+call winget remove Disney.37853FC22B2CE_6rarf9sa4v8jt --accept-source-agreements
+call winget remove SpotifyAB.SpotifyMusic_zpdnekdrzrea0 --accept-source-agreements
 @REM taskkill /f /im OneDrive.exe
-call winget remove Microsoft.OneDrive
+call winget remove Microsoft.OneDrive --accept-source-agreements
 
 @REM  ╭─────────────────────────────────────.★..─╮
 @REM                   *End*
