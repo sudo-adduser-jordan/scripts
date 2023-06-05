d5f2d3d6094086899dc3afbb25f8a07c01c00bd9
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-06-05 15:46:33 -0700

    Update install.sh

--- linux/arch/install.sh
+++ linux/arch/install.sh
@@ -1,13 +1,11 @@
 sudo pacman -S git --noconfirm
+sudo pacman -S gufw --noconfirm
+sudo pacman -S neofetch --noconfirm
 sudo pacman -S base-devel --noconfirm
 sudo pacman -S firefox --noconfirm
-sudo pacman -S gedit --noconfirm
-sudo pacman -S code --noconfirm
-sudo pacman -S gufw --noconfirm
 sudo pacman -S intellij-idea-community-edition --noconfirm
-sudo pacman -S neofetch --noconfirm
+sudo pacman -S code --noconfirm
 # sudo pacman -S --noconfirm
-
 
 
 sudo git clone https://aur.archlinux.org/github-desktop-bin.git  ~/Downloads/github-desktop/
