97249a3ad076274a45c33da218ef9edfe36ec4d4
jordan <108599131+sudo-adduser-jordan@users.noreply.github.com>
2023-11-14 00:26:00 -0800

    Update updateandclean.sh

--- linux/ubuntu/updateandclean.sh
+++ linux/ubuntu/updateandclean.sh
@@ -1,84 +1,15 @@
 #!/bin/bash
+#!/bin/bash 
 
-#         _.-'78o``"`--._
-#     ,o888o.  .o888o,   ''-.
-#   ,88888P  `78888P..______.]
-#  /_..__..----""        __.'
-#  `-._       /""| _..-''
-#      "`-----\  `\
-#              |   ;.-""--..
-#              | ,8o.  o88. `.
-#              `;888P  `788P  :
-#        .o""-.|`-._         ./
-#       J88 _.-/    ";"-P----'
-#       `--'\`|     /  /
-#           | /     |  |
-#           \|     /   |akn
-#            `-----`---'
-
-# "A byte of time is a byte of gold,
-#  but you can’t buy that byte of time,
-#  with a byte of gold.”
-
-# (∩｀-´)⊃━ ☆ﾟ.*･｡ﾟ
-
-# Define colors...
-RED=`tput bold && tput setaf 1`
-GREEN=`tput bold && tput setaf 2`
-YELLOW=`tput bold && tput setaf 3`
-BLUE=`tput bold && tput setaf 4`
-NC=`tput sgr0`
-
-function RED(){
-	echo -e "\n${RED}${1}${NC}"
-}
-function GREEN(){
-	echo -e "\n${GREEN}${1}${NC}"
-}
-function YELLOW(){
-	echo -e "\n${YELLOW}${1}${NC}"
-}
-function BLUE(){
-	echo -e "\n${BLUE}${1}${NC}"
-}
-
-# ╭─────────────────────────────────────.★..─╮
-#           	*Test for Root*
-# ╰─..★.─────────────────────────────────────╯
-
-if [ $UID -ne 0 ]
-then
-	RED "You must run this script as root!" && echo
-	exit
-fi
-
-# ╭─────────────────────────────────────.★..─╮
-#           	*Update and Clean*
-# ╰─..★.─────────────────────────────────────╯
-
+#Update and Clean
 # System Update and Upgrade
-BLUE "Updating and Upgrading System..."
 sudo apt update
 sudo apt install --fix-missing -y
 sudo apt upgrade --allow-downgrades -y
 sudo apt full-upgrade --allow-downgrades -y
 
 # System Clean Up
-BLUE "Cleaning System..."
 sudo apt install -f
 sudo apt autoremove -y
 sudo apt autoclean
 sudo apt clean
-
-# ╭─────────────────────────────────────.★..─╮
-#           	*Confirmation*
-# ╰─..★.─────────────────────────────────────╯
-
-GREEN "Update and Clean completed!"
-
-# ╭─────────────────────────────────────.★..─╮
-#           	 *Snippets*
-# ╰─..★.─────────────────────────────────────╯
-
-# sudo npm install -g npm@latest
-
