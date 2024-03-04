89fef16713186371a7621e7762bd4f2f2abf5b0b
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-04 05:01:10 +0000

    Create mintinstall.sh

---
+++ linux/mint/mintinstall.sh
@@ -0,0 +1,97 @@
+# Debian / Ubuntu / Mint Installer
+
+# APT
+sudo apt install nala -y
+sudo nala update -y
+sudo nala upgrade -y
+sudo nala install tree -y
+sudo nala install g++ -y
+sudo nala install gh -y
+sudo nala install git -y
+sudo nala install nodejs
+sudo nala install mgba-qt -y
+sudo nala install imagemagick -y
+sudo nala install steam-installer -y
+
+#FLATPAKS
+flatpak install melonDS -y
+flatpak install Bottles -y
+
+# PATHS
+$Discord="https://discord.com/api/download?platform=linux&format=deb"
+$Docker="https://desktop.docker.com/linux/main/amd64/139021/docker-desktop-4.28.0-amd64.deb"
+$Downloads="/home/$SUDO_USER/Downloads"
+
+# INSTALL FUNCTIONS
+code_install() {    
+    sudo nala install wget gpg -y
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
+    sudo nala install apt-transport-https -y
+    sudo nala update
+    sudo nala install code -y # or code-insiders
+}
+
+ulauncher_isntall() {
+    sudo add-apt-repository universe -y
+    sudo add-apt-repository ppa:agornostal/ulauncher -y
+    sudo nala update 
+    sudo nala install ulauncher -y
+    # THEME 
+    mkdir -p ~/.config/ulauncher/user-themes
+    git clone https://github.com/sudo-adduser-jordan/ulauncher-mint-y-dark-green.git  ~/.config/ulauncher/user-themes/mint-y-dark-green
+}
+
+github_desktop_install() {
+    wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+    sudo nala update 
+    sudo nala install github-desktop -y
+}
+
+docker_install() {
+    sudo nala install gnome-terminal -y
+    # Add Docker's official GPG key:
+    sudo nala update
+    sudo nala install ca-certificates curl -y
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    # Add the repository to Apt sources:
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo nala update
+    wget -O docker.deb $Docker -P $Downloads
+    sudo nala install $Downloads/docker.deb -y
+
+}
+
+discord_install(){
+    wget -O discord.deb $Discord
+    sudo nala install $Downloads/discord.deb
+}
+
+icons_install(){
+    sudo git clone https://github.com/sudo-adduser-jordan/icons $Downloads/icons
+    for file in $Downloads/icons/*.tar.xz; do tar -Jxf "$file" -C $Downloads; done
+    sudo rm -rf $Downloads/icons
+    mv -v $Downloads/* /home/$SUDO_USER/.icons
+}
+
+
+# INSTALL
+icons_install
+discord_instal
+docker_install
+github_desktop_install
+ulauncher_isntall
+code_install
+
+
+clean_downloads() {
+   sudo rm -r ~/Downloads/*
+}
+
+
