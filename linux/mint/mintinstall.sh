b9f1670de9dbb8d00a3a542d894c3199d1c6540e
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-05 04:07:12 -0800

    Update mintinstall.sh

--- linux/mint/mintinstall.sh
+++ linux/mint/mintinstall.sh
@@ -6,8 +6,6 @@ sudo nala update
 sudo nala upgrade -y
 sudo nala install tree -y
 sudo nala install g++ -y
-# create function for gh
-# sudo nala install gh -y
 sudo nala install git -y
 sudo nala install nodejs -y
 sudo nala install mgba-qt -y
@@ -86,8 +84,17 @@ go_install (){
     go version
 }
 
+gh_install() {
+    sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null 
+    sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg 
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
+    sudo nala update 
+    sudo nala install gh -y
+}
+
 
 # INSTALL
+gh_install
 go_install
 icons_install
 discord_install
