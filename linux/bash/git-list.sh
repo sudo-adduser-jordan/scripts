1d1b16fc49ebf80fe5ce5cac214a0a5996db471d
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-06 07:25:56 -0800

    Add Git List

---
+++ linux/bash/git-list.sh
@@ -0,0 +1,11 @@
+# GitHub CLI api
+# https://cli.github.com/manual/gh_api
+
+### GIT AND PRINT ALL REPOSITORIES ###
+
+# gh api --paginate \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   /user/repos | jq '.[].html_url' | awk '{print $NF}' FS=/ | awk -F\" '{print $1}'
+
+gh api --paginate /user/repos --jq '.[].name' 
