df99c0d7a56b34c2c97e55f05b24b099e399f418
jordan <sudo.sendmail.jordan@gmail.com>
2024-03-22 14:22:15 -0700

    Update

--- linux/git/git-clone-all.sh
+++ linux/git/git-clone-all.sh
@@ -2,7 +2,10 @@
 FILE=repositories.txt
 
 gh auth status
+echo
+
 for RESPOSITORY in `cat $FILE`
 do
     gh repo clone $RESPOSITORY
+    echo
 done
--- linux/git/git-pull-all.sh
+++ linux/git/git-pull-all.sh
@@ -5,7 +5,10 @@ DRIVE=CORSAIR
 FILE=names.txt
 
 gh auth status
+echo
+
 for RESPOSITORY in `cat $FILE`
 do
-  cd $ROOT/$USER/$DRIVE/$RESPOSITORY && pwd && git pull    
+  cd $ROOT/$USER/$DRIVE/$RESPOSITORY && pwd && git pull
+  echo    
 done
